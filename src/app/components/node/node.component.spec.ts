6b22590cb51456d6795f4b5358831c2420043c86
Elie Soueid <eliegebransoued@gmail.com>
2025-07-13 17:31:39 +0200

    test case of node component

--- src/app/components/node/node.component.spec.ts
+++ src/app/components/node/node.component.spec.ts
@@ -1,40 +1,92 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NodeComponent } from './node.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { NodeService } from '../../services/node.service';
+import { MatTree } from '@angular/material/tree';
+import { type node } from '../../services/node.service';
+import { By } from '@angular/platform-browser';
 
-// import { NodeComponent } from './node.component';
+describe('NodeComponent', () => {
+  let component: NodeComponent;
+  let fixture: ComponentFixture<NodeComponent>;
+  let nodeService: jasmine.SpyObj<NodeService>;
+  let mockTree: jasmine.SpyObj<MatTree<node>>;
 
-// describe('NodeComponent', () => {
-//   let component: NodeComponent;
-//   let fixture: ComponentFixture<NodeComponent>;
+  const mockNode = {
+    text: 'Stammdatenverwaltung',
+    iconCls: 'stamm.ico',
+    children: [
+      {
+        text: 'Kundenstamm',
+        call: 'R10ST00001',
+        iconCls: 'prosoz_16.ico',
+        leaf: true,
+      },
+    ],
+  };
 
-//   const mockNode = {
-//     text: '',
-//     iconCls: '',
-//     children: [],
-//   };
+  beforeEach(async () => {
+    nodeService = jasmine.createSpyObj('NodeService', ['isNodeMatch']);
+    mockTree = jasmine.createSpyObj<MatTree<node>>('MatTree', [
+      'isExpanded',
+      'toggle',
+    ]);
 
-//   const nodeInputs = {
-//     isleaf: false,
-//     node: mockNode,
-//     searchValue: '',
-//     tree: undefined,
-//   };
+    await TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
 
-//   const tree = beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [NodeComponent],
-//     }).compileComponents();
+        {
+          provide: nodeService,
+          useValue: nodeService,
+        },
+      ],
+      imports: [NodeComponent],
+    }).compileComponents();
 
-//     fixture = TestBed.createComponent(NodeComponent);
-//     component = fixture.componentInstance;
-//     component.isLeaf = false;
-//     component.node = mockNode;
-//     component.searchValue = '';
-//     component.tree = ;
+    fixture = TestBed.createComponent(NodeComponent);
+    component = fixture.componentInstance;
+    component.isLeaf = false;
+    component.node = mockNode;
+    component.tree = mockTree;
+    component.searchValue = 'pilex';
 
-//     fixture.detectChanges();
-//   });
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     // expect(component).toBeTruthy();
-//   });
-// });
+  it('Render a node', () => {
+    const button = fixture.debugElement.query(By.css('#nodeBtn'));
+    const icon = fixture.debugElement.query(By.css('#nodeIcon'));
+    const span = fixture.debugElement.query(By.css('#nodeSpan'));
+
+    expect(button).toBeTruthy();
+    expect(span).toBeTruthy();
+    expect(icon).toBeTruthy();
+  });
+
+  it('Render a leaf', () => {
+    component.node = {
+      text: 'Kundenstamm',
+      iconCls: 'prosoz_16.ico',
+    };
+
+    component.isLeaf = true;
+
+    fixture.detectChanges();
+
+    const button = fixture.debugElement.query(By.css('#leafBtn'));
+    const span = fixture.debugElement.query(By.css('#leafSpan'));
+    expect(button).toBeTruthy();
+    expect(span).toBeTruthy();
+  });
+
+  it('Expand node', () => {
+    component.node = mockNode;
+    component.isLeaf = false;
+    const button = fixture.debugElement.query(By.css('#nodeBtn')).nativeElement;
+    button.click();
+    expect(mockTree.toggle).toHaveBeenCalled();
+  });
+});
