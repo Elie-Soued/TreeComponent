3cef8ed155863cc51b34490e164ed3cf2230d86d
Elie Soueid <elie.soueid@ogs.de>
2025-08-05 09:15:33 +0200

    refactor: improve readability of the drag service

--- src/app/services/drag.service.ts
+++ src/app/services/drag.service.ts
@@ -1,77 +1,85 @@
 /* eslint-disable @tseslint/prefer-readonly-parameter-types */
-import { Injectable, WritableSignal, inject, signal } from '@angular/core';
+import { Injectable, Renderer2, RendererFactory2, WritableSignal, inject, signal } from '@angular/core';
 import { type Position, type TreeNode } from '../types';
 import { FavoritesService } from './favorites.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DragService {
-  isDragging: boolean = false;
+  /**
+   * It is the value that needs to be substracted from the absolute position of the cursor to ensure a smooth dragging.
+   * Without the offset, the dragged element will jump to the cursor's position while dragging (bad UI)
+   */
+  private elementOffset: Position = { x: 0, y: 0 };
 
-  dragStartPosition: Position = { x: 0, y: 0 };
-
-  currentMousePosition: Position = { x: 0, y: 0 };
-
-  draggedNode: TreeNode | null = null;
-
-  elementOffset: Position = { x: 0, y: 0 };
-
-  private treeElement!: HTMLElement;
-
+  /**
+   * Limits of the dragged element so it doesn't go beyond the tree
+   */
   private boundaries: { left: number; top: number; right: number; bottom: number; } | null = null;
+
+  /**
+   * Absolute mouse position on the screen minus the elementOffset
+   */
+  private currentElementPosition: Position = { x: 0, y: 0 };
+
+  /**
+   * Sharing the element position using signals
+   */
+  draggedPosition: WritableSignal<Position> = signal(this.currentElementPosition);
+
+  /**
+   * Informing other components if the dragged element is above a favorite element (aka if there is an acceptable dropzone)
+   */
+  overFavorite: WritableSignal<boolean> = signal<boolean>(false);
+
+  private isDragging: boolean = false;
+
+  private draggedNode: TreeNode | null = null;
 
   private isFavorite: boolean = false;
 
-  draggedPosition: WritableSignal<Position> = signal({
-    x: 0,
-    y: 0
-  });
-
-  overFavorite: WritableSignal<boolean> = signal<boolean>(false);
+  private tree!: HTMLElement;
 
   private favoriteService: FavoritesService = inject(FavoritesService);
 
+  private renderer: Renderer2;
+
+  constructor (private rendererFactory: RendererFactory2) {
+    this.renderer = this.rendererFactory.createRenderer(null, null);
+  }
+
+
   startDrag (e: MouseEvent, node: TreeNode, draggedElement: HTMLElement, tree: HTMLElement): void {
+    // Initiate the drag
     this.isDragging = true;
     this.draggedNode = node;
-    this.treeElement = tree;
-
-    const rect: DOMRect = draggedElement.getBoundingClientRect();
-    const rectTree: DOMRect = this.treeElement.getBoundingClientRect();
-
-    this.elementOffset = {
-      x: e.clientX - rect.left,
-      y: e.clientY - rect.top
-    };
-    this.boundaries = {
-      left: rectTree.left,
-      top: rectTree.top,
-      right: rectTree.right - rect.width,
-      bottom: rectTree.bottom - rect.height
-    };
-    this.dragStartPosition = { x: rect.left, y: rect.top };
-    this.currentMousePosition = {
-      x: e.clientX - this.elementOffset.x,
-      y: e.clientY - this.elementOffset.y
-    };
-    this.draggedPosition.set(this.currentMousePosition);
+    this.tree = tree;
+    // Set boundaries and position
+    this.getElementPosition(e, draggedElement);
+    this.getBoundaries(draggedElement);
+    this.draggedPosition.set(this.currentElementPosition);
     this.checkFavoriteHover(e);
-    document.addEventListener('mousemove', this.onMouseMove);
-    document.addEventListener('mouseup', this.onMouseUp);
-    document.body.style.userSelect = 'none';
+    // Add event listeners
+    this.renderer.listen('document', 'mousemove', this.onMouseMove);
+    this.renderer.listen('document', 'mouseup', this.onMouseUp);
+    this.renderer.setStyle(document.body, 'user-select', 'none');
   }
 
   onMouseMove = (e: MouseEvent): void => {
     if (!this.isDragging || !this.boundaries) return;
 
+    // Recalculate the element's positioning on the move
     let newX: number = e.clientX - this.elementOffset.x;
     let newY: number = e.clientY - this.elementOffset.y;
 
+    // Making sure the position doesn't go beyond the boundaries
     newX = Math.max(this.boundaries.left, Math.min(this.boundaries.right, newX));
     newY = Math.max(this.boundaries.top, Math.min(this.boundaries.bottom, newY));
-    this.currentMousePosition = { x: newX, y: newY };
-    this.draggedPosition.set(this.currentMousePosition);
+    // Share the position with exterior elements
+    this.currentElementPosition = { x: newX, y: newY };
+    this.draggedPosition.set(this.currentElementPosition);
     this.checkFavoriteHover(e);
   };
 
@@ -82,20 +90,13 @@ export class DragService {
     this.isDragging = false;
     this.draggedNode = null;
     this.overFavorite.set(false);
-    document.removeEventListener('mousemove', this.onMouseMove);
-    document.removeEventListener('mouseup', this.onMouseUp);
-    document.body.style.userSelect = '';
+    this.renderer.listen('document', 'mousemove', this.onMouseMove);
+    this.renderer.listen('document', 'mouseup', this.onMouseUp);
+    this.renderer.setStyle(document.body, 'user-select', '');
   };
 
-  private checkFavoriteHover (e: MouseEvent): void {
-    const elementBelow: Element | null = document.elementFromPoint(e.clientX, e.clientY);
-    const favoriteElement: Element | null | undefined = elementBelow?.closest('.favorite');
 
-    this.isFavorite = Boolean(favoriteElement);
-    this.overFavorite.set(this.isFavorite);
-  }
-
-  handleDrop (e: MouseEvent): void {
+  private handleDrop (e: MouseEvent): void {
     const elementBelow: Element | null = document.elementFromPoint(e.clientX, e.clientY);
     const dropTarget: Element | null | undefined = elementBelow?.closest('app-node');
 
@@ -109,5 +110,39 @@ export class DragService {
           this.favoriteService.dropNode(this.draggedNode, nodeValue);
       }
     }
+  }
+
+  private checkFavoriteHover (e: MouseEvent): void {
+    const elementBelow: Element | null = document.elementFromPoint(e.clientX, e.clientY);
+    const favoriteElement: Element | null | undefined = elementBelow?.closest('.favorite');
+
+    this.isFavorite = Boolean(favoriteElement);
+    this.overFavorite.set(this.isFavorite);
+  }
+
+
+  private getElementPosition (e: MouseEvent, draggedElement: HTMLElement): void {
+    const rect: DOMRect = draggedElement.getBoundingClientRect();
+
+    this.elementOffset = {
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top
+    };
+    this.currentElementPosition = {
+      x: e.clientX - this.elementOffset.x,
+      y: e.clientY - this.elementOffset.y
+    };
+  }
+
+  private getBoundaries (draggedElement: HTMLElement): void {
+    const rect: DOMRect = draggedElement.getBoundingClientRect();
+    const rectTree: DOMRect = this.tree.getBoundingClientRect();
+
+    this.boundaries = {
+      left: rectTree.left,
+      top: rectTree.top,
+      right: rectTree.right - rect.width,
+      bottom: rectTree.bottom - rect.height
+    };
   }
 }
