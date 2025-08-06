a5dd145f38cf7e1005e45fa4a7781dd564870ab0
Elie Soueid <elie.soueid@ogs.de>
2025-08-06 11:33:19 +0200

    feat: add the burger menu to toggle the tree

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
 import { TreeComponent } from './components/tree/tree.component';
 import { SearchInputComponent } from './components/searchinput/searchinput.component';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ TreeComponent, SearchInputComponent ],
+  imports: [ TreeComponent, SearchInputComponent, MatIconModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   searchValue: string | null = null;
 
+  showTree: boolean = false;
+
+  isDisabled: boolean = false;
+
+
   onSearchValueChange (value: string | null): void {
     this.searchValue = value;
+  }
+
+  toggleTree (): void {
+    this.showTree = !this.showTree;
   }
 }
